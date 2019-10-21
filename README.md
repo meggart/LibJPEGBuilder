a7a25ddb71da88e68a62d4c248f6f2b419dae9a9
Fabian Gans <fgans@bgc-jena.mpg.de>
2019-10-21 16:51:24 +0200

    Add autogenerated files to build build_jpeg_9c

---
+++ README.md
@@ -0,0 +1,9 @@
+# BuildJPEG
+
+[![Build Status](https://travis-ci.org/meggart/BuildJPEG.svg?branch=master)](https://travis-ci.org/meggart/BuildJPEG)
+
+This repository builds binary artifacts for the build_jpeg_9c project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/meggart/BuildJPEG/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,57 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "build_jpeg_9c"
+version = v"9.0.0-c"
+
+# Collection of sources required to build build_jpeg_9c
+sources = [
+    "http://www.ijg.org/files/jpegsrc.v9c.tar.gz" =>
+    "650250979303a649e21f87b5ccd02672af1ea6954b911342ea491f351ceb7122",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+./configure --prefix=$prefix --host=$target ./jpeg-9c/
+cd jpeg-9c
+./configure --prefix=$prefix --host=$target
+make
+make install
+exit
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:i686, libc=:glibc),
+    Linux(:x86_64, libc=:glibc),
+    Linux(:aarch64, libc=:glibc),
+    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    Linux(:powerpc64le, libc=:glibc),
+    Linux(:i686, libc=:musl),
+    Linux(:x86_64, libc=:musl),
+    Linux(:aarch64, libc=:musl),
+    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
+    MacOS(:x86_64),
+    FreeBSD(:x86_64),
+    Windows(:i686),
+    Windows(:x86_64)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libjpeg", :jpeg)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
